352a32a557062ffcd8a0e6120a7dd5996576e80e
Jadetrieu <enamjade0921@icloud.com>
2021-10-12 13:08:06 -0500

    tuesday

--- src/main/java/base/Practice.java
+++ src/main/java/base/Practice.java
@@ -1,8 +1,21 @@
 package base;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Practice {
-    public static void main(String[] args){
-        String x = "test";
-        System.out.println(x);
+    public static void main(String[] args) {
+
+        WebDriver driver=new ChromeDriver();
+        driver.get("http://qa.taltektc.com/home/login");
+
+        driver.findElement(By.name("email")).sendKeys("Anhngocjade2303@gmail.com");
+        driver.findElement(By.name("password")).sendKeys("Enajade2303");
+        driver.findElement(By.xpath("//input[@value='Log In']")).click();
+      //  try {
+     //       assertEquals(driver.findElement(By.xpath("//form[@id='profile_form']/legend")).getText(), "Welcome to TalentTek");
+      //  } catch (Error e) {
+     //       verificationErrors.append(e.toString());
+        }
     }
 }
---
+++ src/test/java/stepDef/myProfile_step.java
@@ -0,0 +1,11 @@
+package stepDef;
+
+import io.cucumber.java.en.Then;
+
+public class myProfile_step {
+
+    @Then("I should be able to successfully log in")
+    public void iShouldBeAbleToSuccessfullyLogIn() {
+    }
+
+}
